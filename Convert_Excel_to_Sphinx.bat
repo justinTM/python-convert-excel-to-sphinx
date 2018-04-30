6d2eb46665613ba1fe56c41196a9e524180e9879
Justin Mai <justintmai@gmail.com>
2018-04-30 16:59:38 +0000

    Changing script filename

--- Convert_Excel_to_Sphinx.bat
+++ Convert_Excel_to_Sphinx.bat
@@ -1,2 +1,2 @@
 
-@echo on && echo Running python script && python hello_world.py && cd ..\Sphinx && echo Moving all rst files to Sphinx directory && copy /Y "..\Python Scripts\*.rst" ..\Sphinx && cd ..\Sphinx && echo Building Sphinx documentation from rsts && make html && cd "..\Python Scripts"
+@echo on && echo Running python script && python populate_templates_from_sheets.py && cd ..\Sphinx && echo Moving all rst files to Sphinx directory && copy /Y "..\Python Scripts\*.rst" ..\Sphinx && cd ..\Sphinx && echo Building Sphinx documentation from rsts && make html && cd "..\Python Scripts"
